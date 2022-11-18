648f9f1b7182b9edfb08e713f150fefc38dee284
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-18 18:02:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// var inp = +prompt("Enter a positive number")
+// console.log("number Value" + inp)
+// console.log(Math.floor(inp))
+// console.log(Math.ceil(inp))
+// console.log(Math.round(inp))
+
+// var inp = prompt("Enter a negative number");
+// console.log("number Value" + inp)
+// console.log(Math.floor(inp))
+// console.log(Math.ceil(inp))
+// console.log(Math.round(inp))
+
+// var inp = prompt("")
+var num = 3.45124;
+console.log("Actual value is"+num);
+console.log("Round Off value is" +Math.round(num));
+console.log("Ceil value is" + Math.ceil(num));
+console.log("Floor value is" + Math.floor(num));
+
+var num = -2.673;
+console.log("Actual value is"+num);
+console.log("Round of Value is" + Math.round(num));
+console.log("Ceil value is "+ Math.ceil(num));
+console.log("Floor value is "+ Math.floor(num))
