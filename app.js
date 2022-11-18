208a0fd3f09b76631f095ebc40548a0aa2a529e1
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-18 18:02:25 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,14 +11,54 @@
 // console.log(Math.round(inp))
 
 // var inp = prompt("")
-var num = 3.45124;
-console.log("Actual value is"+num);
-console.log("Round Off value is" +Math.round(num));
-console.log("Ceil value is" + Math.ceil(num));
-console.log("Floor value is" + Math.floor(num));
+// var num = 3.45124;
+// console.log("Actual value is"+num);
+// console.log("Round Off value is" +Math.round(num));
+// console.log("Ceil value is" + Math.ceil(num));
+// console.log("Floor value is" + Math.floor(num));
 
-var num = -2.673;
-console.log("Actual value is"+num);
-console.log("Round of Value is" + Math.round(num));
-console.log("Ceil value is "+ Math.ceil(num));
-console.log("Floor value is "+ Math.floor(num))
+// var num = -2.673;
+// console.log("Actual value is"+num);
+// console.log("Round of Value is" + Math.round(num));
+// console.log("Ceil value is "+ Math.ceil(num));
+// console.log("Floor value is "+ Math.floor(num))
+
+// var num = Math.floor(Math.random()*10)
+// console.log(num)
+// var inp = +prompt("Entera any number ")
+// console.log(inp)
+// if(num===inp){
+//     alert("You Are win")
+// }else{
+//     alert("try Again")
+// }
+
+
+// var num = Math.floor(Math.random()*10)
+// document.write("The Dice value is " + num +"<br>")
+// var num = Math.floor(Math.random()*10)
+
+// document.write("The Dice value is " + num +"<br>")
+// var num = Math.floor(Math.random()*10)
+
+// document.write("The Dice value is " + num +"<br>")
+// var num = Math.floor(Math.random()*10)
+
+// document.write("The Dice value is " + num +"<br>")
+// var num = Math.floor(Math.random()*10)
+
+// document.write("The Dice value is " + num+"<br>")
+
+// var num = Math.floor(Math.random()*10)
+
+// document.write("The Dice value is " + num+"<br>")
+
+
+// var num = Math.floor(Math.random()*3)
+// if (num === 2){
+//     alert("you chose a head")
+// }else if(num === 1){
+//     alert("You chose a tail")
+// }else{
+//     alert("Match fixed")
+// }
